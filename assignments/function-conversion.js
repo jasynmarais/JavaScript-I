91e72abb70a5dc39e52838c72c25757337461e7a
Jasyn Marais <jasynmarais@gmail.com>
2019-08-19 14:37:37 +0200

    function conversion 3

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -21,6 +21,9 @@ console.log(anotherFunction('This is a param'));
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1,param2)=>(param1+param2);
+
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
